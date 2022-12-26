9dc564804337c2cff1caf22302474c75e706e0e9
Ccolina03 <colinaperu2003@gmail.com>
2022-12-26 14:15:02 -0700

    Adding Replace functionality for Cart

--- context/shopContext.js
+++ context/shopContext.js
@@ -15,6 +15,21 @@ export default function shopProvider({children}) {
         const checkout = await createCheckout(newItem.id, newItem.variantQuantity)
         setCheckoutId(checkout.id)
         setCheckoutUrl(checkout.webUrl)
+
+        localStorage.setItem('checkout_id', JSON.stringify([newItem, checkout]))
+    } else {
+        let newCart = {...cart} 
+        cart.map(item => { //check if item exists to increase and pass to NewCart
+            if (item.id === newItem.id) {
+                item.variantQuantity++
+                newCart = {...cart}
+            }
+            else 
+            {
+                newCart= {...cart, newItem} //copy last cart if id did not match
+            }
+        })
+        setCart(newCart) //update after adding Cart feature
     }
     }
     
