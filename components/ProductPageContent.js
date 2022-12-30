866cc50adc27c4b240fdd3bb97e1c574c3ca0bc9
Ccolina03 <colinaperu2003@gmail.com>
2022-12-29 18:30:12 -0700

    Adding a Reccomended products section and a description for each product

--- components/ProductPageContent.js
+++ components/ProductPageContent.js
@@ -2,6 +2,7 @@ import Image from "next/dist/client/image"
 import ProductForm from "./ProductForm"
 import SwiperCore, {Navigation, Pagination} from 'swiper'
 import {Swiper, SwiperSlide} from 'swiper/react'
+import RecommendedList from "./RecommendedList"
 
 
 export default function ProductPageContent({product}) {
@@ -18,7 +19,9 @@ export default function ProductPageContent({product}) {
 
     SwiperCore.use([Navigation, Pagination])
 return (
-            <div className="flex flex-col justify-center items-center space-y-8 md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8 max-w-6xl w-11/12 mx-auto ">
+        <div>
+
+         <div className="flex flex-col justify-center items-center space-y-8 md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8 max-w-6xl w-11/12 mx-auto ">
             <div className="w-full max-w-md border bg-white rounded-2xl overflow-hidden shadow-lg md:w-1/2">
                 <div className="relative h-96 w-full">
                    <Swiper
@@ -33,6 +36,9 @@ return (
                 </div>
             </div>
             <ProductForm product={product} />
+        </div>
+            <p className="w-11/12 max-w-3xl pt-16 mx-auto space-y-8 md:space-x-4 lg:space-x-8">{product.description}</p>
+            <RecommendedList current={product.id} products={product.collections.edges[0].node.products.edges}/>
         </div>
     )
 }
