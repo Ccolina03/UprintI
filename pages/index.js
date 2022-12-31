6d8d516a23e54932929e201aafc3294bdebd9a9d
Ccolina03 <colinaperu2003@gmail.com>
2022-12-31 10:09:19 -0700

    Adding the basic SEO for the website SEO to be 100

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
 import {getProductsInCollection} from '../lib/shopify'
 import ProductList from '../components/ProductList'
 import Hero from '../components/Hero'
+import Head from 'next/head'
+
+
 export default function Home({ products }) {
   return (
     <div>
+      <Head> 
+        <title> UPrintI eCommerce Store</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+        <meta name="description" content='Ecommerce store created with Next.js, TailwindCSS, GraphQL and Shopify with primary mission to help my mom sell our childhood toys 
+        and get a future revenue to fund our education. Addtional functionalities would be facilitating entrepreneurships to display and sell their products and services' />
+        <meta property="og:title" content="UPrinti eCommerce Store" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://uprint-i.vercel.app/" />
+        <meta property="og:image" content="https://uprint-i.vercel.app/images/costaverde.jpg" />
+        <meta property="og:description" 
+  content='Ecommerce store created with Next.js, TailwindCSS, GraphQL and Shopify with primary mission to help my mom sell our childhood toys 
+  and get a future revenue to fund our education. Addtional functionalities would be facilitating entrepreneurships to display and sell their products and services' />
+     <meta property="og:locale" content="en_US" />
+     <meta property="og:site_name" content="UPrintI eCommerce Store" />
+
+      </Head>
       <Hero/>
       <ProductList products={products}/>
     </div>
