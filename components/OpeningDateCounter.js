1b770c13ed777eef558055d0a4c154fde3bf3727
Carlos Colina <107608945+Ccolina03@users.noreply.github.com>
2025-04-07 09:38:16 -0600

    Update OpeningDateCounter.js

--- components/OpeningDateCounter.js
+++ components/OpeningDateCounter.js
@@ -10,7 +10,7 @@ export default function OpeningDateCounter() {
 
   //Change will happen once 
   useEffect(() => {
-    const target = new Date("08/08/2025 00:00:00") //target date
+    const target = new Date("08/08/2026 00:00:00") //target date
     const interval = setInterval(() => {
       const now = new Date().getTime() //Current time
       const countDownDate = target.getTime() //target time
